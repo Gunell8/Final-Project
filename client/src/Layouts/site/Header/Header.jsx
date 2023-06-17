d97547fb424348f62643b24f3e6206850b78e841
Gunel <126647673+Gunell8@users.noreply.github.com>
2023-06-17 21:33:15 +0400

    back data upgraded

--- client/src/Layouts/site/Header/Header.jsx
+++ client/src/Layouts/site/Header/Header.jsx
@@ -17,7 +17,7 @@ export const Header = () => {
             <li><NavLink to="/gallery">Gallery</NavLink></li>
             <li><NavLink to="/event">Event</NavLink></li>
             <li><NavLink to="/blog">Blog</NavLink></li>
-            <li><NavLink to="/shop">Shop</NavLink></li>
+            <li><NavLink to="/shop">Products</NavLink></li>
             <li><NavLink to="/projects">Projects</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
           
