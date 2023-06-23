b7dcfc45c81a444052db3b4cc1fbe46eeb1a65b4
Gunel <126647673+Gunell8@users.noreply.github.com>
2023-06-23 17:58:51 +0400

    dropdown menyu added

--- client/src/Layouts/site/Header/Header.jsx
+++ client/src/Layouts/site/Header/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import logo from "../../../assests/images/62f7950839fad2082789a0be_artgram (1).svg"
 import "./style.scss"
 import { NavLink } from 'react-router-dom'
+import { Menu, Dropdown } from "antd";
 export const Header = () => {
+
+
   return (
     <header>
         <div className='container'>
@@ -14,11 +17,43 @@ export const Header = () => {
           <ul>
             <li><NavLink to="">Home</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/gallery">Gallery</NavLink></li>
-            <li><NavLink to="/event">Event</NavLink></li>
-            <li><NavLink to="/blog">Blog</NavLink></li>
+              <li>
+
+            <Dropdown
+                      overlay={
+                        <Menu>
+                          <Menu.Item key="0">
+                            <NavLink to={"/blog"}>Blog</NavLink>
+                          </Menu.Item>
+                          <Menu.Item key="1">
+                            <NavLink to={"/event"}>
+                              Event
+                            </NavLink>
+                          </Menu.Item>
+                          <Menu.Item key="2">
+                            <NavLink to={"/projects"}>
+                              Projects
+                            </NavLink>
+                          </Menu.Item>
+                          <Menu.Item key="3">
+                            <NavLink to={"/gallery"}>
+                              Gallery
+                            </NavLink>
+                          </Menu.Item>
+                        </Menu>
+                      }
+                      trigger={["hover"]}
+                    >
+                      <NavLink
+                        className="ant-dropdown-link"
+                        onClick={(e) => e.preventDefault()}
+                        to={"/"}
+                      >
+                        Pages <span className="menu-arrow"></span>
+                      </NavLink>
+                    </Dropdown>
+              </li>
             <li><NavLink to="/shop">Products</NavLink></li>
-            <li><NavLink to="/projects">Projects</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
             <li><NavLink to="/wishlist">Wishlist</NavLink></li>
           
